1fe6fd9b717816627fbb119deebc49e752151d71
Vuaya Timamputu <vuayapaulo@gmail.com>
2024-07-31 15:21:40 +0200

    Updates

---
+++ precache-manifest.1cd1f862988c82656a28f1b54071f3af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2c35f9b265eb16ade46e41451d6588",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "2202662bbae2aa5a5b4d",
+    "url": "/RoboFriends/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "f4601afea3314bfdad29",
+    "url": "/RoboFriends/static/css/main.75afd252.chunk.css"
+  },
+  {
+    "revision": "2202662bbae2aa5a5b4d",
+    "url": "/RoboFriends/static/js/2.5fc5a6d4.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/RoboFriends/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "f4601afea3314bfdad29",
+    "url": "/RoboFriends/static/js/main.5cbfb16d.chunk.js"
+  },
+  {
+    "revision": "743f2bf4b0c07794547b",
+    "url": "/RoboFriends/static/js/runtime~main.d425f0ad.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
